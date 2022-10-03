e4bf9c667e287add1d1ef42b69e57cd8a89ecf8c
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2022-10-03 15:18:29 +0300

    Same Tree solution

--- Trees/Same Tree/Program.cs
+++ Trees/Same Tree/Program.cs
@@ -1,1 +1,33 @@
 ﻿using Same_Tree;
+
+var solver = new Solution();
+
+// // Case 1
+// var nA1 = new TreeNode(1);
+// var nA2 = new TreeNode(2);
+
+// var nB1 = new TreeNode(1);
+// var nB2 = new TreeNode(2);
+
+// nA1.left = nA2;
+// nB1.right = nB2;
+
+// System.Console.WriteLine($"Is same: {solver.IsSameTree(nA1, nB1)}");
+
+
+// Case 2
+var nA1 = new TreeNode(1);
+var nA2 = new TreeNode(2);
+var nA3 = new TreeNode(3);
+
+var nB1 = new TreeNode(1);
+var nB2 = new TreeNode(2);
+var nB3 = new TreeNode(3);
+
+nA1.left = nA2;
+nA1.right = nA3;
+
+nB1.left = nB2;
+nB1.right = nB3;
+
+System.Console.WriteLine($"Is same: {solver.IsSameTree(nA1, nB1)}");
--- Trees/Same Tree/Solution.cs
+++ Trees/Same Tree/Solution.cs
@@ -2,9 +2,26 @@ namespace Same_Tree
 {
     public class Solution
     {
-        public bool IsSameTree(TreeNode p, TreeNode q)
+        public bool IsSameTree(TreeNode? p, TreeNode? q)
         {
+            bool isSame = true;
+            TraverseTree(q, p, ref isSame);
+            return isSame;
+        }
 
+        private void TraverseTree(TreeNode? p, TreeNode? q, ref bool isSame)
+        {
+            if (p is null && q is null) return;
+            if ((p is null && q is not null) || (q is null && p is not null) || p.val != q.val)
+            {
+                isSame = false;
+                return;
+            }
+
+            TraverseTree(p.left, q.left, ref isSame);
+            TraverseTree(p.right, q.right, ref isSame);
+
+            return;
         }
     }
 }
