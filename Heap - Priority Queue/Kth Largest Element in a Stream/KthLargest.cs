9050b26a50a12f03fc03a9e5036504b5f709c2a1
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2022-11-16 11:18:09 +0200

    Initial commit

---
+++ Heap - Priority Queue/Kth Largest Element in a Stream/KthLargest.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kth_Largest_Element_in_a_Stream
+{
+    public class KthLargest
+    {
+        public KthLargest(int k, int[] nums)
+        {
+
+        }
+
+        public int Add(int val)
+        {
+            return 0;
+        }
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.Add(val);
+ */
---
+++ Heap - Priority Queue/Kth Largest Element in a Stream/KthLargestTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kth_Largest_Element_in_a_Stream
+{
+    internal class KthLargestTests
+    {
+        private KthLargest? Mock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Mock = new();
+        }
+
+        [Test]
+        public void Case1()
+        {
+
+        }
+    }
+}
