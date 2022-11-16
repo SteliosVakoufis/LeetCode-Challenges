06f010dbcebd4d67264d76cf089780052cd8114b
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2022-11-16 13:08:38 +0200

    Initial Commit

---
+++ Heap - Priority Queue/Last Stone Weight/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last_Stone_Weight
+{
+    public class Solution
+    {
+        public int LastStoneWeight(int[] stones)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Heap - Priority Queue/Last Stone Weight/SolutionTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last_Stone_Weight
+{
+    public class SolutionTests
+    {
+        private Solution? _solution;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _solution = new();
+        }
+
+        [Test]
+        public void Case1()
+        {
+            int[] input = { 2, 7, 4, 1, 8, 1 };
+
+            Assert.AreEqual(_solution?.LastStoneWeight(input), 1);
+        }
+
+        [Test]
+        public void Case2()
+        {
+            int[] input = { 1 };
+
+            Assert.AreEqual(_solution?.LastStoneWeight(input), 1);
+        }
+
+    }
+}
