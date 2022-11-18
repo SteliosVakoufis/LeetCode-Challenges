6de7b17530f023e5281e1047aa21a7d9c19be473
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2022-11-18 13:44:00 +0200

    Initial Commit

---
+++ 1-D Dynamic Programming/Min Cost Climbing Stairs/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min_Cost_Climbing_Stairs
+{
+    public class Solution
+    {
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            return 0;
+        }
+    }
+}
---
+++ 1-D Dynamic Programming/Min Cost Climbing Stairs/SolutionTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min_Cost_Climbing_Stairs
+{
+    public class SolutionTests
+    {
+        private Solution _solution;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _solution = new();
+        }
+
+        [Test]
+        public void Case1()
+        {
+            int[] costs = { 10, 15, 20 };
+
+            Assert.AreEqual(15, _solution.MinCostClimbingStairs(costs));
+        }
+
+        [Test]
+        public void Case2()
+        {
+            int[] costs = { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
+
+            Assert.AreEqual(6, _solution.MinCostClimbingStairs(costs));
+        }
+    }
+}
