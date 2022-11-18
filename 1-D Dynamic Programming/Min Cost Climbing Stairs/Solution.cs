5672a61fc34abb06ab93cc1889627a34332d6224
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2022-11-18 15:12:26 +0200

    Brute-Force solution

--- 1-D Dynamic Programming/Min Cost Climbing Stairs/Solution.cs
+++ 1-D Dynamic Programming/Min Cost Climbing Stairs/Solution.cs
@@ -8,9 +8,32 @@ namespace Min_Cost_Climbing_Stairs
 {
     public class Solution
     {
-        public int MinCostClimbingStairs(int[] cost)
+        private int _min;
+
+        public int MinCostClimbingStairs(int[] costs)
         {
-            return 0;
+            _min = int.MaxValue;
+
+            MinCost(costs, 0, 0);
+
+            return _min;
+        }
+
+        private void MinCost(int[] costs, int index, int cost)
+        {
+            if (cost >= _min)
+                return;
+
+            if (index >= costs.Length - 1)
+            {
+                if (cost < _min)
+                    _min = cost;
+                return;
+            }
+
+            MinCost(costs, index + 1, cost + costs[index]);
+            index++;
+            MinCost(costs, index + 1, cost + costs[index]);
         }
     }
 }
