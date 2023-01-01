9ad4465209c65ab884bc25a254c003a33782bf77
Stelios Vakoufis <stelios.vakoufis@gmail.com>
2023-01-01 13:15:27 +0200

    renamed to include csharp as a suffix

--- 1-D Dynamic Programming/Min Cost Climbing Stairs - csharp/Solution.cs
+++ 1-D Dynamic Programming/Min Cost Climbing Stairs - csharp/Solution.cs
@@ -9,31 +9,40 @@ namespace Min_Cost_Climbing_Stairs
     public class Solution
     {
         private int _min;
+        private Dictionary<int, int> _solutions = new();
 
         public int MinCostClimbingStairs(int[] costs)
         {
             _min = int.MaxValue;
 
-            MinCost(costs, 0, 0);
+            MinCost(costs, -1, 0);
 
             return _min;
+            //return _solutions.MinBy(kvp => kvp.Value).Value;
         }
 
         private void MinCost(int[] costs, int index, int cost)
         {
-            if (cost >= _min)
-                return;
+            if (_solutions.ContainsKey(index))
+            {
+                cost += _solutions[index];
+                index = costs.Count();
+            }
 
-            if (index >= costs.Length - 1)
+            if (index >= costs.Length - 2)
             {
                 if (cost < _min)
                     _min = cost;
                 return;
             }
 
-            MinCost(costs, index + 1, cost + costs[index]);
             index++;
-            MinCost(costs, index + 1, cost + costs[index]);
+            MinCost(costs, index, cost + costs[index]);
+            index++;
+            MinCost(costs, index, cost + costs[index]);
+
+            //_solutions.TryAdd(index, cost);
+            _solutions.TryAdd(index, cost);
         }
     }
 }
